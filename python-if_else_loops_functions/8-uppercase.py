3b4298d31bd0a95fff42b8e6e1d775e0f3d17691
quliyevamin1227-ctrl <quliyevamin1227@gmail.com>
2026-07-30 08:00:21 +0000

    Fix uppercase function

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-def print_uppercase_twice(str=""):
-    """Prints a string 2 times in uppercase followed by a new line."""
-    str = str(str)
-    result = ""
-
-    for char in str:
-        if 'a' <= char <= 'z':
-            result += chr(ord(char) - 32)
+def uppercase(str):
+    res = ""
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            res += chr(ord(c) - 32)
         else:
-            result += char
-
-    print("{0}{0}".format(result))
+            res += c
+    print("{}".format(res))
