f7ea42e4a993abd005a5fdfe0d34917aefe04440
quliyevamin1227-ctrl <quliyevamin1227@gmail.com>
2026-07-31 13:23:43 +0000

    Add task bilmirem neche5

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matric:
+    for row in matrix:
         for i in range(len(row)):
             if i == len(row) - 1:
                 print("{:d}".format(row[i]), end="")
