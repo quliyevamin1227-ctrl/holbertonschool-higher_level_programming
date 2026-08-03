fe028f8916728e3b11ed6e3b96eb46552166ca89
quliyevamin1227-ctrl <quliyevamin1227@gmail.com>
2026-08-03 07:19:26 +0000

    Add task1

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that defines a Square class."""
+
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size):
+        """Initialize a square with a private size."""
+        self.__size = size
