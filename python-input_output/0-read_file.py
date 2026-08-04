a94ba307b13cb5df6f5cf79f8763b6af03edec4d
Gquliyevamin1227-ctrl <quliyevamin1227@gmail.com.com>
2026-08-04 06:58:19 +0000

    Add task0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""Module for reading a text file."""
+
 
 def read_file(filename=""):
+    """Read a UTF-8 text file and print it to stdout."""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
