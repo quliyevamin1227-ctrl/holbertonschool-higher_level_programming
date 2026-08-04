90eb9c3803bb5ac35d5b53ab558e97f86a2ee906
Gquliyevamin1227-ctrl <quliyevamin1227@gmail.com.com>
2026-08-04 07:08:24 +0000

    Add task2

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module for appending text to a file."""
+
+
+def append_write(filename="", text=""):
+    """Append text to a UTF-8 file."""
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
