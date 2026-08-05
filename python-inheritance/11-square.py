ebdfa6b1528c070429a330239bc30a26f8a05b87
quliyevamin1227-ctrl <quliyevamin1227@gmail.com>
2026-08-05 15:53:31 +0000

    Add task11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module that defines Square."""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Square class."""
+
+    def __init__(self, size):
+        """Initialize Square."""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Return the area of the square."""
+        return super().area()
+
+    def __str__(self):
+        """Return the square description."""
+        return "[Square] {}/{}".format(self.__size, self.__size)
