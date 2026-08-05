cd7b90e8da3295ac10dcaee68244aecff4366a40
quliyevamin1227-ctrl <quliyevamin1227@gmail.com>
2026-08-05 15:50:22 +0000

    Add task8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module that defines Rectangle."""
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class."""
+
+    def __init__(self, width, height):
+        """Initialize Rectangle."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
