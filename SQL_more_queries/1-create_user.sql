9e9720d97a52d4f106c14f3d3f5923ca94011d9f
quliyevamin1227-ctrl <quliyevamin1227@gmail.com>
2026-08-09 16:51:05 +0000

    Add task01

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Creates user_0d_1 if it does not exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
