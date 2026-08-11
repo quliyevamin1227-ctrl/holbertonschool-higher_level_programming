404ee4c9f4a20153d93ab915e926f9a11715cab5
quliyevamin1227-ctrl <quliyevamin1227@gmail.com>
2026-08-11 07:38:14 +0000

    Add task00

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cur = db.cursor()
+    cur.execute("SELECT id, name FROM states ORDER BY id ASC")
+
+    for state in cur.fetchall():
+        print(state)
+
+    cur.close()
+    db.close()
