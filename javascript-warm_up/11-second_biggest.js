1b30dc5da2020159d92c93d291ac971840585068
quliyevamin1227-ctrl <quliyevamin1227@gmail.com>
2026-08-19 13:35:53 +0000

    Add task11

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2).map(Number);
+
+if (args.length < 2) {
+  console.log(0);
+} else {
+  args.sort((a, b) => b - a);
+
+  let biggest = args[0];
+  let secondBiggest = null;
+
+  for (const num of args) {
+    if (num < biggest) {
+      secondBiggest = num;
+      break;
+    }
+  }
+
+  console.log(secondBiggest === null ? 0 : secondBiggest);
+}
