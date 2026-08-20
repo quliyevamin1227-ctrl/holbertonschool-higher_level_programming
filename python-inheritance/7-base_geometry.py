abb8f99e3a750e4b0b9018ea2a09314f8d109c7d
quliyevamin1227-ctrl <quliyevamin1227@gmail.com>
2026-08-20 08:43:55 +0000

    Add BaseGeometry integer_validator method and tests

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -6,7 +6,7 @@ class BaseGeometry:
     """Represent base geometry."""
 
     def area(self):
-        """Not implemented."""
+        """Raise an Exception indicating that area is not implemented."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
